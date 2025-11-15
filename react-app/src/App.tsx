da4d3d43fc572091e796e33d7e1e552c85bc613c
ericmelz <eric@emelz.com>
2025-11-15 13:41:30 -0800

    CSS modules

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,15 @@
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 import {useState} from "react";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
     const [showAlert, setShowAlert] = useState(false);
 
     return (
         <>
+            <ListGroup items={["cat", "dog", "horse"]} heading={"Animals"}
+                       onSelectItem={() => console.log("Selected")}/>
             {showAlert && <Alert onDismiss={() => setShowAlert(false)}>Hello World!</Alert>}
             <Button color='danger' label="Click me" onClick={() => setShowAlert(true)}/>
         </>
--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import styles from './ListGroup.module.css'
 
 interface Props {
     items: string[];
@@ -6,18 +7,21 @@ interface Props {
     onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading, onSelectItem }: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
     const [selectedIndex, setSelectedIndex] = useState(-1)
 
     return <>
         <h1>{heading}</h1>
-        <ul className="list-group">
-            {items.length == 0 && <p>No items</p>}
+        {items.length == 0 && <p>No items</p>}
+        <ul className={[styles.listGroup, styles.container].join(' ')}>
             {items.map((item, index) =>
                 <li
                     className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
                     key={index}
-                    onClick={() => {setSelectedIndex(index); onSelectItem(item)}}
+                    onClick={() => {
+                        setSelectedIndex(index);
+                        onSelectItem(item)
+                    }}
                 >{item}</li>)}
         </ul>
     </>;
