f72be28dba8f73f39a3d0f7100d244514d629aac
ericmelz <eric@emelz.com>
2025-11-17 14:28:06 -0800

    state challenge

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,7 +5,28 @@ import ListGroup from "./components/ListGroup/ListGroup";
 import Like from "./components/Like";
 
 function App() {
-    return <Like onClick={() => console.log('clicked')}/>
+    const [cart, setCart] = useState({
+        discount: .1,
+        items: [
+            {id: 1, title: 'Product 1', quantity: 1},
+            {id: 2, title: 'Product 2', quantity: 2}
+        ]
+    })
+    const handleClick = () => {
+        setCart({
+            ...cart,
+            items: cart.items.map(item =>
+                item.id === 1 ? {...item, quantity: item.quantity + 1} :
+                    item)
+        })
+    }
+
+    return (
+        <>
+            {cart.items.map(item => <p key={item.id}>{item.title}: {item.quantity}</p>)}
+            <button onClick={handleClick}>Add 1 to {cart.items[0].title}</button>
+        </>
+    )
 }
 
 export default App;
