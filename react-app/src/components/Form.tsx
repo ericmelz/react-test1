f725863bb766a4e628a50c480c1ea3df93075e52
ericmelz <eric@emelz.com>
2025-11-18 21:03:07 -0800

    work on forms

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,10 +1,20 @@
 // @flow
 import * as React from 'react';
 import {FieldValues, useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
 
-type Props = {};
+const schema = z.object({
+    name: z.string().min(3, {message: 'Name must be at least 3 characters'}),
+    age: z.number({ invalid_type_error: 'Age field is required' }).min(18)
+});
+
+
+type FormData = z.infer<typeof schema>;
+
+
 export const Form = (props: Props) => {
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm<FormData>({resolver: zodResolver(schema)});
 
     const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -16,13 +26,15 @@ export const Form = (props: Props) => {
                     <input {...register('name')}
                            type="text" className="form-control"
                            id="exampleFormControlName" placeholder="<NAME>"/>
+                    {errors.name && <p className="text-danger">{errors.name.message}</p>}
                 </div>
                 <div className="mb-3">
                     <label htmlFor="numberControlInput1" className="form-label">Age</label>
-                    <input {...register('age')} type="number" className="form-control" id="numberControlInput1"
+                    <input {...register('age', {valueAsNumber: true})} type="number" className="form-control" id="numberControlInput1"
                            placeholder="123"/>
+                    {errors.age && <p className="text-danger">{errors.age.message}</p>}
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" disabled={!isValid} className="btn btn-primary">Submit</button>
             </form>
         </>
     );
