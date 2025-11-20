fff1b5d9068386f953c62b9d986686ffee38aa27
ericmelz <eric@emelz.com>
2025-11-20 13:09:42 -0800

    deletion

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -32,17 +32,34 @@ function App() {
                 setError((error as AxiosError).message);
                 setIsLoading(false);
             })
-            // .finally(() => setIsLoading(false));
+        // .finally(() => setIsLoading(false));
 
         return () => controller.abort();
     }, []);
+
+    function deleteUser(user: User) {
+        const originalUsers = [...users];
+        setUsers(users.filter(u => u.id !== user.id));
+        axios.delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+            .catch(error => {
+                setError(error.message);
+                setUsers(originalUsers);
+            });
+    }
 
     return (
         <>
             {isLoading && <div className="spinner-border"></div>}
             {error && <p className="text-danger">{error}</p>}
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            <ul className="list-group">
+                {users.map(user =>
+                    <li key={user.id} className="list-group-item d-flex justify-content-between align-items-center">
+                        {user.name}
+                        <button className="btn btn-outline-danger"
+                                onClick={() => deleteUser(user)}>
+                            Delete
+                        </button>
+                    </li>)}
             </ul>
         </>)
 }
