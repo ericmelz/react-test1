aa2026e1865470cebb22e7ed577ba9f3015bedd3
ericmelz <eric@emelz.com>
2025-11-20 12:13:43 -0800

    fetch data error handling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,16 +7,24 @@ interface User {
     username: string;
     email: string;
 }
+
 function App() {
     const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-            .then(res => setUsers(res.data));
+            .then(res => setUsers(res.data))
+            .catch(err => setError(err.message));
     }, []);
-    return <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>
+
+    return (
+        <>
+        {error && <p className="text-danger">{error}</p>}
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>)
 }
 
 export default App;
