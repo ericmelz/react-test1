98ebe0e36e3c67ea642283823846c7cf55390d06
ericmelz <eric@emelz.com>
2025-11-20 12:47:21 -0800

    is loading

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -11,24 +11,35 @@ interface User {
 function App() {
     const [users, setUsers] = useState<User[]>([]);
     const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
 
+        setIsLoading(true);
         axios
             .get('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
-            .then(response => setUsers(response.data))
+            .then(response => {
+                setUsers(response.data);
+                setIsLoading(false);
+                setError(
+                    null
+                )
+            })
             .catch(error => {
                 if (error instanceof CanceledError)
                     return;
                 setError((error as AxiosError).message);
-            });
+                setIsLoading(false);
+            })
+            // .finally(() => setIsLoading(false));
 
         return () => controller.abort();
     }, []);
 
     return (
         <>
+            {isLoading && <div className="spinner-border"></div>}
             {error && <p className="text-danger">{error}</p>}
             <ul>
                 {users.map(user => <li key={user.id}>{user.name}</li>)}
